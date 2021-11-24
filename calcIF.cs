97105186650d3a9c69fafab399d091ee11f8ccef
LNZdude <77962306+LNZdude@users.noreply.github.com>
2021-11-25 00:05:07 +0200

    Add files via upload

---
+++ calcIF.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Clear();
+                double firstvalue, secondvalue;
+                string action;
+                try
+                {
+                    Console.WriteLine("Введите число 1");
+                    firstvalue = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите число 2");
+                    secondvalue = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите корректные данные");
+                    Console.ReadLine();
+                    continue;
+                }
+                
+
+                Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+                action = Console.ReadLine();
+
+                if (action == "+")
+                {
+                    Console.WriteLine(firstvalue + secondvalue);
+                }
+                else if (action == "-")
+                {
+                    Console.WriteLine(firstvalue - secondvalue);
+                }
+                else if (action == "*")
+                {
+                    Console.WriteLine(firstvalue * secondvalue);
+                }
+                else if (action == "/")
+                {
+                    if (secondvalue == 0)
+                        Console.WriteLine(0);
+                    else
+                    {
+                        Console.WriteLine(firstvalue / secondvalue);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! неизвестное действие!");
+                    Console.WriteLine("Ошибка! неизвестное действие!");
+                }
+
+
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
