4517f56b70d2005b30fe4dc20f22664b65fb8d2f
LNZdude <77962306+LNZdude@users.noreply.github.com>
2021-11-25 00:03:26 +0200

    Add files via upload

---
+++ CurrencyConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+/*
+
+КОНВЕРТЕР ВАЛЮТ В ГРИВНЕ  
+
+*/
+namespace LessonsSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Clear();
+                string EnterCurrency;           
+                string CourseCurrvalue0;
+                string CourseCurrvalue1;
+                string CourseCurrvalue2;
+                string AdminMenu; 
+                double secondvalue;           
+                double thirdvalue;
+                double result;
+
+                try
+                {
+
+                    Console.WriteLine(" Введите курс Валюты евро: ");
+
+                    EnterCurrency = Console.ReadLine();
+
+                    secondvalue = Convert.ToDouble(EnterCurrency);
+                    
+                    Console.WriteLine(" Введите кол-во Евро которое хотите обменять: ");
+
+                    CourseCurrvalue0 = Console.ReadLine();
+
+                    thirdvalue = Convert.ToDouble(CourseCurrvalue0);
+                    result = thirdvalue * secondvalue;
+                    Console.WriteLine(" Кол-во в Гривне: " + result);
+                }
+                
+                catch (Exception)
+                {
+                    Console.WriteLine("Введите корректные данные!");
+                    continue;
+                }
+
+                Console.ReadLine();
+
+            }
+           
+
+
+        }
+    }
+}
